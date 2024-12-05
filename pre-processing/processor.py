eb85a2bd87705530d3c78416cd5b5383a3925221
Hayden <66452827+HaydenCJuneau@users.noreply.github.com>
2024-12-05 11:23:44 -0500

    Monthly Data View

--- pre-processing/processor.py
+++ pre-processing/processor.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import re
 
+exportCols = ["EXPIREDATE", "ISSUEDATE", "WARNINGTYPE", "POLYGON", "MEAN"]
+
 """
 Polygon should parse into this structure:
 
@@ -52,25 +54,44 @@ def polyMean(raw: list[list[list]]):
         parsed.append(sums.tolist())
 
     return parsed  
-        
 
-def main(year: int):
-    df = pd.read_csv(rf"C:\Users\hayde\Documents\UNCC-Work-Local\ITCS-6121\VisFInalProject\data\raw\warn-{year}.csv")
+
+def main():
+    # Holds 12 dataframes representing each month
+    months = [pd.DataFrame(columns=[]) for _ in range(12)]
+
+    total = 0
     
-    df["WARNINGTYPE"] = df["WARNINGTYPE"].apply(parseWarning)
-    df["POLYGON"] = df["POLYGON"].apply(parsePolygon)
-    df["MEAN"] = df["POLYGON"].apply(polyMean)
+    for year in range(2001, 2017):
+        df = pd.read_csv(rf"C:\Users\hayde\Documents\UNCC-Work-Local\ITCS-6121\VisFInalProject\data\raw\warn-{year}.csv")
+        
+        df["WARNINGTYPE"] = df["WARNINGTYPE"].apply(parseWarning)
+        df["POLYGON"] = df["POLYGON"].apply(parsePolygon)
+        df["MEAN"] = df["POLYGON"].apply(polyMean)
 
-    print(df["WARNINGTYPE"].unique())
+        for i, mdf in enumerate(months):
+            selector = df["ISSUEDATE"].str[5:7].astype(int) == i + 1
+            monthRows = df.loc[selector]
+            months[i] = pd.concat([mdf, monthRows], ignore_index=True, sort=False)
 
-    df.to_csv(
-        rf"C:\Users\hayde\Documents\UNCC-Work-Local\ITCS-6121\VisFInalProject\data\warn-{year}-parsed.csv",
-        index=False,
-        columns=["EXPIREDATE", "ISSUEDATE", "WARNINGTYPE", "POLYGON", "MEAN"]
-    )
+        total += len(df.index)
+        del df # Free Memory Taken by DataFrame
+
+    mtotal = 0
+    for i, mdf in enumerate(months):
+        print(mdf["WARNINGTYPE"].unique())
+        mtotal += len(mdf.index)
+
+        mdf.to_csv(
+            rf"C:\Users\hayde\Documents\UNCC-Work-Local\ITCS-6121\VisFInalProject\data\monthly\warn-month-{i + 1}.csv",
+            index=False,
+            columns=["EXPIREDATE", "ISSUEDATE", "WARNINGTYPE", "POLYGON", "MEAN"]
+        )
+
+    # For testing. Check that no data was lost
+    print(f"dft: {total}, mdft: {mtotal}, same: {total == mtotal}")
 
 
 if __name__ == "__main__":
-    for i in range(2001, 2017):
-        main(i)
+    main()
         
